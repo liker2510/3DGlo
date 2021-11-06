5ccdff6ed4044f924c5cab443fa0542cf3407989
Ilker Karakurt <i_love_history96@mail.ru>
2021-11-06 13:26:10 +0500

    Выполнил домашнее задание 25 урока

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,41 +1,31 @@
+import { animate } from "./helpers";
+
 const modal = () => {
     const modal = document.querySelector('.popup');
     const buttons = document.querySelectorAll('.popup-btn');
-
-    function openOpacity() {
-        modal.style.cssText = "display: block;" + "opacity: 0;";
-        let opacity = 0;
-        while (opacity <= 1) {
-            (function (opacity) {
-                setTimeout(function () { modal.style.opacity = opacity; }, 60 + opacity * 1000);
-            })(opacity);
-            opacity += 0.1;
-        }
-    }
-    // function closeOpacity() {
-    //     let opacity = 0;
-    //     while (opacity >= 0) {
-    //         (function (opacity) {
-    //             setTimeout(function () { modal.style.opacity = opacity; }, 60 + opacity * 1000);
-    //         })(opacity);
-    //         opacity -= 0.1;
-    //     }
-    // }
 
     buttons.forEach(btn => {
         btn.addEventListener('click', () => {
             const screenWidth = window.screen.width;
             modal.style.cssText = "display: block;";
             if (screenWidth >= '767') {
-                openOpacity();
+                animate({
+                    duration: 1200,
+                    timing(timeFraction) {
+                        return 1 - Math.sin(Math.acos(timeFraction));
+                    },
+                    draw(progress) {
+                        modal.style.cssText = "display: block;" + "opacity: 0;";
+                        modal.style.opacity = progress * 100 + '%';
+                    }
+                  });
             }
         })
-    })
+    })  
 
     modal.addEventListener('click', (e) => {
         if (!e.target.closest('.popup-content') || e.target.classList.contains('popup-close')) {
             modal.style.cssText = "display: none;";
-            // closeOpacity();
         }
     })
 
